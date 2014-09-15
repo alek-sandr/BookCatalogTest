714adc49e65a5b318bfeb655f06cfc4af4b176b5
aleksandr452777204 <aleksandr452777204@gmail.com>
2014-09-15 04:08:29 +0300

    added db dump, small config and messages fixes

--- resources/db.sql
+++ resources/db.sql
@@ -1,5 +1,141 @@
-CREATE DATABASE IF NOT EXISTS bookcatalog;
+-- MySQL dump 10.13  Distrib 5.5.20, for Win32 (x86)
+--
+-- Host: localhost    Database: bookcatalog
+-- ------------------------------------------------------
+-- Server version	5.5.20-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `bookcatalog`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `bookcatalog` /*!40100 DEFAULT CHARACTER SET utf8 */;
 CREATE USER bookcatalog@localhost identified BY 'secret';
 CREATE USER bookcatalog@127.0.0.1 identified BY 'secret';
 GRANT ALL privileges ON bookcatalog.* TO bookcatalog@localhost;
 GRANT ALL privileges ON bookcatalog.* TO bookcatalog@127.0.0.1;
+
+USE `bookcatalog`;
+
+--
+-- Table structure for table `authors`
+--
+
+DROP TABLE IF EXISTS `authors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `authors` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `FIRSTNAME` varchar(30) NOT NULL,
+  `LASTNAME` varchar(30) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `authors`
+--
+
+LOCK TABLES `authors` WRITE;
+/*!40000 ALTER TABLE `authors` DISABLE KEYS */;
+INSERT INTO `authors` VALUES (1,'Котофей','Мохнатый'),(2,'Крокодил','Зубастый'),(3,'Псина','Породистая');
+/*!40000 ALTER TABLE `authors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `book_author`
+--
+
+DROP TABLE IF EXISTS `book_author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book_author` (
+  `BOOK_ID` bigint(20) NOT NULL,
+  `AUTHOR_ID` bigint(20) NOT NULL,
+  KEY `FK_a9803a0hl54hxs7equ58bskvk` (`AUTHOR_ID`),
+  KEY `FK_7jnf52vun75t9tm4x2cqs6add` (`BOOK_ID`),
+  CONSTRAINT `FK_7jnf52vun75t9tm4x2cqs6add` FOREIGN KEY (`BOOK_ID`) REFERENCES `books` (`ID`),
+  CONSTRAINT `FK_a9803a0hl54hxs7equ58bskvk` FOREIGN KEY (`AUTHOR_ID`) REFERENCES `authors` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book_author`
+--
+
+LOCK TABLES `book_author` WRITE;
+/*!40000 ALTER TABLE `book_author` DISABLE KEYS */;
+INSERT INTO `book_author` VALUES (1,1),(2,1),(2,3),(3,2);
+/*!40000 ALTER TABLE `book_author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `books` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `DESCRIPTION` longtext NOT NULL,
+  `TITLE` varchar(255) NOT NULL,
+  `YEAR` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES (1,'про котов','Коты',2000),(2,'про котов и не котов','Коты и собаки',2001),(3,'про содержание крокодила в домашних условиях','Почему крокодил лучше кошки и собаки',2009);
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `LOGIN` varchar(30) NOT NULL,
+  `PASSWORD` varchar(30) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-09-15  4:06:04
